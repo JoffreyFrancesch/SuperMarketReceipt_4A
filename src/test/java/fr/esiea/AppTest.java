05fdbf2b0a5788fc212a6d7de457ed0f4026ad9e
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-01-15 11:24:53 +0100

    Create Maven Project and Change JUNIT

---
+++ src/test/java/fr/esiea/AppTest.java
@@ -0,0 +1,17 @@
+package fr.esiea;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    @Test
+    public void testApp()
+    {
+        Assertions.assertThat(true).isTrue();
+    }
+}
