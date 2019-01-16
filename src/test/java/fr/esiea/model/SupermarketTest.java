88e6506dde2c122611e41886ca6c98e10c49bc86
Francois Delville <fran.delville@gmail.com>
2019-01-16 13:48:12 +0100

    [TEST] Add ReceiptPrinter Test

--- src/test/java/fr/esiea/model/SupermarketTest.java
+++ src/test/java/fr/esiea/model/SupermarketTest.java
@@ -1,5 +1,6 @@
 package fr.esiea.model;
 
+import fr.esiea.ReceiptPrinter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +23,24 @@ public class SupermarketTest {
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(4.975);
+    }
+
+    @Test
+    public void testReceiptPrinter() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(new ReceiptPrinter().printReceipt(receipt)).isNotBlank();
     }
 }
