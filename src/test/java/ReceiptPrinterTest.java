1a4f481dc4cb137ccf9f1fd4cb4d9f97a2952de3
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-02-06 23:52:26 +0100

    Add receiptPrinter test case

---
+++ src/test/java/ReceiptPrinterTest.java
@@ -0,0 +1,29 @@
+import fr.esiea.ReceiptPrinter;
+import fr.esiea.model.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ReceiptPrinterTest {
+
+    @Test
+    public void testPrinter(){
+        SupermarketCatalog catalog = new FakeCatalog();
+        ShoppingCart cart = new ShoppingCart();
+
+        Product wine = new Product("wine", ProductUnit.Kilo);
+        Product beer = new Product("beer",ProductUnit.Kilo);
+        catalog.addProduct(wine, 10.00);
+        catalog.addProduct(beer, 5.00);
+
+
+        Teller teller = new Teller(catalog);
+        cart.addItemQuantity(wine,1);
+        cart.addItemQuantity(beer,2);
+
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+        ReceiptPrinter printer = new ReceiptPrinter();
+
+        Assertions.assertThat(printer.printReceipt(receipt)).isNotBlank();
+    }
+}
