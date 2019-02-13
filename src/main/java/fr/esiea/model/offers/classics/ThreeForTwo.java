494ab41b3eea28dccdec2f400aa4763370266f54
mehdi.soltana@yahoo.fr <Chergui1997>
2019-02-13 20:56:13 +0100

    add three for two offer

---
+++ src/main/java/fr/esiea/model/offers/classics/ThreeForTwo.java
@@ -0,0 +1,25 @@
+package fr.esiea.model.offers.classics;
+
+import fr.esiea.model.market.Discount;
+import fr.esiea.model.market.catalog.SupermarketCatalog;
+import fr.esiea.model.market.product.Product;
+import fr.esiea.model.offers.Offer;
+
+import java.util.Map;
+
+public class ThreeForTwo implements Offer {
+    @Override
+    public Product[] getProducts() {
+        return new Product[0];
+    }
+
+    @Override
+    public Discount getDiscount() {
+        return null;
+    }
+
+    @Override
+    public Map<Product, Double> makeDiscount(Map<Product, Double> productQuantities, SupermarketCatalog catalog) {
+        return null;
+    }
+}
