d88cc4186fd510758a51aa078c13393f5d3685af
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-02-13 19:41:25 +0100

    implement missing methods

--- src/test/java/fr/esiea/model/FakeCatalog.java
+++ src/test/java/fr/esiea/model/FakeCatalog.java
@@ -17,4 +17,20 @@ public class FakeCatalog implements SupermarketCatalog {
     public double getUnitPrice(Product p) {
         return this.prices.get(p.getName());
     }
+
+    @Override
+    public void removeProduct(String name) {
+        products.remove(name);
+        prices.remove(name);
+    }
+
+    @Override
+    public Map<String, Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public Map<String, Double> getPrices() {
+        return prices;
+    }
 }
