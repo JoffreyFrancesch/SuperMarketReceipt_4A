33b2d764c5466907353bc67609251b3055b968eb
mehdi.soltana@yahoo.fr <Chergui1997>
2019-02-13 21:10:00 +0100

    Ajout du contenue de la classe

--- src/main/java/fr/esiea/model/offers/classics/ThreeForTwo.java
+++ src/main/java/fr/esiea/model/offers/classics/ThreeForTwo.java
@@ -8,18 +8,42 @@ import fr.esiea.model.offers.Offer;
 import java.util.Map;
 
 public class ThreeForTwo implements Offer {
+
+    public final Product product;
+    private final double argument;
+    private Discount discount;
+
+    public ThreeForTwo(Product product, double argument) {
+        this.product = product;
+        this.argument = argument;
+    }
+
+
     @Override
     public Product[] getProducts() {
-        return new Product[0];
+        return new Product[]{product};
     }
 
     @Override
     public Discount getDiscount() {
-        return null;
+        return discount;
     }
 
     @Override
     public Map<Product, Double> makeDiscount(Map<Product, Double> productQuantities, SupermarketCatalog catalog) {
-        return null;
+        double base_quantity = productQuantities.get(product);
+        double base_unitPrice = catalog.getUnitPrice(product);
+        int base_quantity_int = (int) base_quantity;
+
+        int x = base_quantity_int / 3;
+
+        if (base_quantity_int > 2) {
+            double discountAmount = base_quantity * base_unitPrice - ((x * 2 * base_unitPrice) + base_quantity_int % 3 * base_unitPrice);
+            discount = new Discount(product, "3 for 2", discountAmount);
+        }
+
+        productQuantities.put(product,(double)base_quantity_int%3);
+
+        return productQuantities;
     }
 }
