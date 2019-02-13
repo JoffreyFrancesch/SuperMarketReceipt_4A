cf6281db7643568cb031fed7396366820a47305f
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-02-13 19:40:57 +0100

    add two methods

--- src/main/java/fr/esiea/model/SupermarketCatalog.java
+++ src/main/java/fr/esiea/model/SupermarketCatalog.java
@@ -1,8 +1,15 @@
 package fr.esiea.model;
 
+import java.util.Map;
+
 public interface SupermarketCatalog {
     void addProduct(Product product, double price);
 
     double getUnitPrice(Product product);
 
+    void removeProduct(String name);
+
+    Map<String, Product> getProducts();
+
+    Map<String, Double> getPrices();
 }
