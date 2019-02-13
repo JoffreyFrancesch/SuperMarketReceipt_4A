4623a59d352d6b28f6631509155481b4fba9b01f
ChoukrounWilliam <choukroun@et.esiea.fr>
2019-02-13 14:00:47 +0100

    Refactoring Two For Amount Class

--- src/main/java/fr/esiea/model/offers/TwoForAmount.java
+++ src/main/java/fr/esiea/model/offers/TwoForAmount.java
@@ -1,4 +1,35 @@
 package fr.esiea.model.offers;
 
-public class TwoForAmount {
+import fr.esiea.model.Discount;
+import fr.esiea.model.Product;
+import fr.esiea.model.SupermarketCatalog;
+
+import java.util.Map;
+
+public class TwoForAmount implements Offer {
+
+    public final Product product;
+    public final double argument;
+    private Discount discount;
+
+    public TwoForAmount(Product product, double argument) {
+        this.product = product;
+        this.argument = argument;
+    }
+
+    @Override
+    public Product[] getProducts() {
+        return new Product[]{product};
+    }
+
+    @Override
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    //TODO
+    @Override
+    public Map<Product, Double> makeDiscount(Map<Product, Double> productQuantities, SupermarketCatalog catalog) {
+        return null;
+    }
 }
