6afe6200de8cd73c1f4dbcc4ac5ff213f35e8fe5
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-02-15 20:26:05 +0100

    makeDiscount for Percent Bundle

--- src/main/java/fr/esiea/model/offers/bundles/PercentBundle.java
+++ src/main/java/fr/esiea/model/offers/bundles/PercentBundle.java
@@ -2,10 +2,13 @@ package fr.esiea.model.offers.bundles;
 
 import fr.esiea.model.market.Discount;
 import fr.esiea.model.market.catalog.SupermarketCatalog;
+import fr.esiea.model.market.product.ProductUnit;
 import fr.esiea.model.offers.Offer;
 import fr.esiea.model.market.product.Product;
 
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class PercentBundle extends AbstractBundle implements Offer {
 
@@ -28,9 +31,29 @@ public class PercentBundle extends AbstractBundle implements Offer {
         return discount;
     }
 
-    //TODO NEED TO BE IMPLEMENTED
     @Override
-    public Map<Product, Double> makeDiscount(Map<Product, Double> productQuantities, SupermarketCatalog catalog) {
-        return null;
+    public Map<Product, Double> makeDiscount(Map<Product, Double> items, SupermarketCatalog catalog) {
+
+        boolean presentQuantities = true;
+        for(Map.Entry<Product,Integer> product : products.entrySet()){
+            if(product.getValue() > items.get(product.getKey())){
+                presentQuantities = false;
+                break;
+            }
+        }
+
+        if(presentQuantities){
+            int numberOfXs = getNumberOfPacks(products,items);
+
+            BiFunction<Integer,Double, Double> offerFunction = (X, Y) -> X * Y * numberOfXs * (100 - argument)/100;
+
+            double discountTotal = getTotalDiscount(products,items,catalog,numberOfXs,offerFunction);
+
+            String name = products.keySet().stream().map(Product::getName).collect(Collectors.joining( " & " ));
+            Product p = new Product(name, ProductUnit.Each);
+            discount = new Discount(p,  "Bundle Percent Offer for " + argument, discountTotal);
+        }
+
+        return items;
     }
 }
