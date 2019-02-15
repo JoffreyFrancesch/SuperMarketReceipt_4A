82b7e211075fb9ab4c5382ea45ee9b02464c16ce
JoffreyFrancesch <franceschini@et.esiea.fr>
2019-02-15 20:17:23 +0100

    Create test after refactoring

--- src/test/java/fr/esiea/model/ShoppingCartTest.java
+++ src/test/java/fr/esiea/model/ShoppingCartTest.java
@@ -3,8 +3,15 @@ package fr.esiea.model;
 import fr.esiea.model.market.catalog.SupermarketCatalog;
 import fr.esiea.model.market.product.Product;
 import fr.esiea.model.market.product.ProductUnit;
+import fr.esiea.model.offers.bundles.PercentBundle;
+import fr.esiea.model.offers.classics.Percent;
+import fr.esiea.model.offers.classics.ThreeForTwo;
+import fr.esiea.model.offers.classics.TwoForAmount;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ShoppingCartTest {
@@ -31,27 +38,98 @@ public class ShoppingCartTest {
 
     @Test
     public void ThreeForTwoRefactoring(){
-        //TODO NEED TO BE IMPLEMENTED
+        Product ketchup = new Product("ketchup", ProductUnit.Each);
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Teller teller = new Teller(catalog);
+        ShoppingCart cart = new ShoppingCart();
+
+        catalog.addProduct(ketchup,3);
+        cart.addItemQuantity(ketchup,3);
+
+        teller.addSpecialOffer(new ThreeForTwo(ketchup,10));
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(6);
     }
 
     @Test
     public void TenPercentRefactoring(){
-        //TODO NEED TO BE IMPLEMENTED
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Teller teller = new Teller(catalog);
+        ShoppingCart cart = new ShoppingCart();
+
+        catalog.addProduct(apples, 10);
+        teller.addSpecialOffer(new Percent(apples, 10));
+        cart.addItem(apples);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(9);
     }
 
     @Test
     public void TwoForAmountRefactoring(){
-        //TODO NEED TO BE IMPLEMENTED
+        Product wine = new Product("wine", ProductUnit.Each);
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Teller teller = new Teller(catalog);
+        ShoppingCart cart = new ShoppingCart();
+
+        catalog.addProduct(wine, 10);
+        teller.addSpecialOffer(new TwoForAmount(wine, 15));
+        cart.addItemQuantity(wine,2);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(15);
     }
 
     @Test
-    public void FifForAmountRefactoring(){
-        //TODO NEED TO BE IMPLEMENTED
+    public void FiveForAmountRefactoring(){
+        Product wine = new Product("wine", ProductUnit.Each);
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Teller teller = new Teller(catalog);
+        ShoppingCart cart = new ShoppingCart();
+
+        catalog.addProduct(wine, 10);
+        //teller.addSpecialOffer(new FiveForAmount(wine,35));
+        cart.addItemQuantity(wine,5);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(35);
     }
 
     @Test
     public void PercentBundleRefactoring(){
-        //TODO NEED TO BE IMPLEMENTED
+        Product mayo = new Product("mayo", ProductUnit.Each);
+        Product ketchup = new Product("ketchup", ProductUnit.Each);
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Teller teller = new Teller(catalog);
+        ShoppingCart cart = new ShoppingCart();
+
+        catalog.addProduct(ketchup, 1);
+        catalog.addProduct(mayo, 1);
+
+        Map<Product,Integer> products = new HashMap<Product,Integer>();
+        products.put(ketchup,2);products.put(mayo,1);
+
+        teller.addSpecialOffer(new PercentBundle(products,50));
+
+
+        cart.addItemQuantity(ketchup, 1);
+        cart.addItemQuantity(mayo,2);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+        double current = receipt.getTotalPrice();
+
+        Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(1.50);
     }
 
     @Test
