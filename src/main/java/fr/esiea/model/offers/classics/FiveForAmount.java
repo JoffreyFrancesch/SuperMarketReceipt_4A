699954f199b9bc97c4b13975339faec154d00e09
kevinberdah <berdah@et.esiea.fr>
2019-02-19 11:19:49 +0100

    Refactoring methods for FiveForAmount Offer

--- src/main/java/fr/esiea/model/offers/classics/FiveForAmount.java
+++ src/main/java/fr/esiea/model/offers/classics/FiveForAmount.java
@@ -20,16 +20,29 @@ public class FiveForAmount implements Offer {
 
     @Override
     public Product[] getProducts() {
-        return new Product[0];
+        return new Product[]{product};
     }
 
     @Override
     public Discount getDiscount() {
-        return null;
+        return discount;
     }
 
     @Override
     public Map<Product, Double> makeDiscount(Map<Product, Double> productQuantities, SupermarketCatalog catalog) {
-        return null;
+        double quantity = productQuantities.get(product);
+        int quantityAsInt = (int) quantity;
+
+        int numberOfXs = quantityAsInt / 5;
+
+        if(quantityAsInt >= 5){
+            double unitPrice = catalog.getUnitPrice(product);
+
+            double discountTotal = unitPrice * quantity - (argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+            discount = new Discount(product,"5 for " + argument, discountTotal);
+        }
+
+        productQuantities.put(product,(double)quantityAsInt % 5);
+        return productQuantities;
     }
 }
